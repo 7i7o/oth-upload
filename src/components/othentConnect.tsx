b3ffeb274cff461575485be530118a919db201d0
7i7o <mparij@gmail.com>
2023-08-04 10:24:55 -0300

    update to arweaveWallet object

--- src/components/othentConnect.tsx
+++ src/components/othentConnect.tsx
@@ -12,37 +12,39 @@ declare global {
     interface Window {
         othentInstalled: boolean;
         othentLoaded: boolean;
-        initOthent: (params?: { API_ID: string }) => Promise<boolean>;
+        // initOthent: (params?: { API_ID: string }) => Promise<boolean>;
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        othent: any;
+        arweaveWallet: { walletName: string; walletVersion: string } & any;
+        // othent: any;
     }
 }
 
 const installOthentMsg = 'Please, Install & Enable Othent Mobile Safari Extension and try again';
 
 const OthentConnect = () => {
+    const [walletLoaded, setWalletLoaded] = useState(false);
     const [userDetails, setUserDetails] = useState<{ given_name: string } | null>(null);
     const [loginClicked, setLoginClicked] = useState(false);
     const [logoutClicked, setLogoutClicked] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const getOthent = async () => {
-        const installed = window.othentInstalled;
-        console.log('Installed: ', installed);
+    const getOthent = () => {
+        // const installed = window.othentInstalled;
+        // console.log('Installed: ', installed);
 
-        if (!installed) {
-            setErrorMessage(installOthentMsg);
+        // if (!installed) {
+        //     setErrorMessage(installOthentMsg);
+        //     return null;
+        // }
+
+        // const loaded = await window.initOthent();
+
+        if (!walletLoaded) {
+            setErrorMessage('Othent wallet not loaded yet, please try again later');
             return null;
         }
 
-        const loaded = await window.initOthent();
-
-        if (!loaded) {
-            setErrorMessage('Could not initialize Othent Extension, please try again later');
-            return null;
-        }
-
-        return window.othent;
+        return window.arweaveWallet;
     };
 
     const tryLogin = async () => {
@@ -101,20 +103,38 @@ const OthentConnect = () => {
         setTimeout(() => setErrorMessage(''), 5000);
     }, [errorMessage]);
 
+    // Listen for the arweaveWalletLoaded DOM CustomEvent
+    useEffect(() => {
+        const handleArweaveWalletLoaded = (e: Event) => {
+            // Called when browser triggers a custom event
+            if (e.type === 'arweaveWalletLoaded') {
+                console.log('From React: Arweave Wallet Loaded!!!');
+                setWalletLoaded(true);
+            } else console.log('From React: Some other event');
+        };
+
+        window.addEventListener('arweaveWalletLoaded', handleArweaveWalletLoaded);
+        return () => {
+            window.removeEventListener('arweaveWalletLoaded', handleArweaveWalletLoaded);
+        };
+    }, []);
+
     return (
         <>
-            {!userDetails && (
+            {!userDetails && walletLoaded && (
                 <button
-                    className="flex cursor-pointer flex-row items-center justify-center rounded-lg bg-[#2375EF] px-4 py-2 text-white hover:shadow-md hover:shadow-[#2375EFcc] active:shadow-sm active:shadow-[#2375EFcc] disabled:bg-[#3385ff]"
+                    className="flex cursor-pointer flex-row items-center justify-center rounded-lg bg-[#2375EF] px-4 py-2 text-white hover:shadow-md hover:shadow-[#2375EFcc] active:shadow-sm active:shadow-[#2375EFcc] disabled:bg-[#888]"
                     // eslint-disable-next-line @typescript-eslint/no-misused-promises
                     onClick={tryLogin}
-                    disabled={loginClicked}>
+                    disabled={loginClicked && !walletLoaded}>
                     <img src="/favicon-inverted.svg" alt="Othent" className="flex w-6 pr-[1px]" />
                     thent&nbsp;Connect
                 </button>
             )}
 
             <span className="text-red-600">{errorMessage}</span>
+            {/* {!walletLoaded && <span className="text-red-600">"Wallet still not loaded"</span>}
+            {walletLoaded && <span className="text-green-600">"Wallet loaded!"</span>} */}
 
             {userDetails && (
                 <div className="flex w-full flex-col gap-2">
